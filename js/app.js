941e6bc2ddd5b41fb54e92ffa572440da473a2b5
Muhammad Anas <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2024-11-28 18:58:22 +0500

    responsive

--- js/app.js
+++ js/app.js
@@ -19,3 +19,17 @@ $(".goToTopBtn a").click(()=>{
   $("html, body").animate({scrollTop: 0}, 100);
   return false;
 })
+// HAMBURGER 
+$(".header-burger img").click(()=> {
+  $(".header-mobile").addClass("open");
+})
+$(".header-close-btn img").click(()=> {
+  $(".header-mobile").removeClass("open");
+})
+// HAMBURGER
+// FILE UPLOAD
+$("#attachment").change((e)=>{
+  const file = e.target.files[0];
+  const fileName = file.name;
+  $(".upload-file-name").text(fileName);
+})
